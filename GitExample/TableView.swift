591c693798fc41952d2cecea23ba5672e2e58585
cladendas <rvdagi05@yandex.ru>
2020-05-24 17:30:32 +0300

    2 какое-то изменение с добавлением нового файла

---
+++ GitExample/TableView.swift
@@ -0,0 +1,14 @@
+//
+//  TableView.swift
+//  GitExample
+//
+//  Created by cladendas on 24.05.2020.
+//  Copyright © 2020 cladendas. All rights reserved.
+//
+
+import UIKit
+
+///что-то типа делает
+class TableView: UITableView {
+
+}
