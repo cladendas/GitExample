a5c55742d90e4c74f5561794078d5c947ea351eb
cladendas <rvdagi05@yandex.ru>
2020-05-24 17:28:06 +0300

    1 какое-то изменение

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("Всем привет)")
+        if 2 > 0 {
+            print("Всем привет)")
+        }
     }
 }
